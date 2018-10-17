1f971e41c0e58f083e729dc9b83805aac143f516
Dawid <majorczyk.dawid@gmail.com>
2018-10-17 09:54:15 +0200

    Add function basen on localStorage

--- Lab1/scripts/script.js
+++ Lab1/scripts/script.js
@@ -20,3 +20,22 @@ var x = setInterval(function () {
         year = year + 1;
     }
 }, 1000);
+
+var buttonHeaderChanger = document.querySelector('button');
+var header = document.querySelector('h1');
+
+function changeHeader() {
+    var customHeader = prompt('Enter your header');
+    localStorage.setItem('header', customHeader);
+    header.textContent = customHeader;
+}
+
+buttonHeaderChanger.onclick = function () {
+    changeHeader();
+}
+
+if (!localStorage.getItem('header')) {
+    changeHeader();
+} else {
+    header.textContent = localStorage.getItem('header');
+}
